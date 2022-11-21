ced92328b60dca001384cee7bc724923ece1fb72
Praveen Kumar <praveen170596@gmail.com>
2022-11-21 08:27:48 +0530

    created constructor for the ProductService class

--- src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/controller/ProductController.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/controller/ProductController.java
@@ -7,6 +7,17 @@
 
 package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.controller;
 
+import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ProductController {
 
+    ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 }
