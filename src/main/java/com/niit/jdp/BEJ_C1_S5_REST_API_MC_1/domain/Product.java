f0675bb3018e6d805c14d391a09439de0e8f5a2d
Praveen Kumar <praveen170596@gmail.com>
2022-11-21 07:54:54 +0530

    created attributes for the product class

--- src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/domain/Product.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/domain/Product.java
@@ -7,5 +7,16 @@
 
 package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+
+    @Id
+    private int productId;
+    private String productName;
+    private String manufacturer;
+
 }
+
