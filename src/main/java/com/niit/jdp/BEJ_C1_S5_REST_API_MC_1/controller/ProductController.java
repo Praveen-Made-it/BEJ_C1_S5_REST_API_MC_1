6873032fbe46977bc61b68059e08cf8e0be1af78
Praveen Kumar <praveen170596@gmail.com>
2022-11-21 08:33:21 +0530

    created postmapping

--- src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/controller/ProductController.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/controller/ProductController.java
@@ -7,7 +7,13 @@
 
 package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.controller;
 
+import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.domain.Product;
 import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +23,17 @@ public class ProductController {
 
     ProductService productService;
 
+    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @PostMapping(value = "/product")
+    public ResponseEntity<?> insertProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.CREATED);
+    }
+
+
 }
+
+
