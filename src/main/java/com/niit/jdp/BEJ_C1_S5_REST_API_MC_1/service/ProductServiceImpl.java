5b233dd72ed19891349088eb406c4d25e9744c25
Praveen Kumar <praveen170596@gmail.com>
2022-11-21 08:21:53 +0530

    iplemented the methods from the ProductService Interface

--- src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/service/ProductServiceImpl.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/service/ProductServiceImpl.java
@@ -7,12 +7,25 @@
 
 package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.service;
 
+import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.domain.Product;
 import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.repository.ProductRepository;
 
-public class ProductServiceImpl {
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
     ProductRepository productRepository;
 
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return null;
     }
 }
