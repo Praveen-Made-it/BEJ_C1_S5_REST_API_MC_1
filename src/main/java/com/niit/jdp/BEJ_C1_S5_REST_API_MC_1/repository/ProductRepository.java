ddf7c1b617eb743136d742969f113472047c03ac
Praveen Kumar <praveen170596@gmail.com>
2022-11-21 08:14:48 +0530

    created ProductService interface class

--- src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/repository/ProductRepository.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.repository;
 
-public interface ProductRepository {
+import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 }
---
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.service;
+
+public interface ProductService {
+}
