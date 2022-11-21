f81653cde91a60738c46634ac1b5b3dcc3887ee5
Praveen Kumar <praveen170596@gmail.com>
2022-11-21 08:18:17 +0530

    created methods to add product and fetch all products from the database

--- src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/service/ProductService.java
+++ src/main/java/com/niit/jdp/BEJ_C1_S5_REST_API_MC_1/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.service;
 
+import com.niit.jdp.BEJ_C1_S5_REST_API_MC_1.domain.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    Product addProduct(Product product);
+
+    List<Product> getAllProduct();
+
 }
